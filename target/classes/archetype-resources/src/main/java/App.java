3cf56fb46a5f508bbc29a21cabd1ae162b16ebba
JamesFoxChen <25717594@qq.com>
2016-02-25 12:01:28 +0800

    init

---
+++ target/classes/archetype-resources/src/main/java/App.java
@@ -0,0 +1,13 @@
+package $com.james.J_SpringService;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
